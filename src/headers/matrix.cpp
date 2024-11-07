57a3f4e1e934ee57686717799d68887ef9c836b7
Projacktor <1234xl.com@gmail.com>
2024-11-07 13:07:39 +0300

    refactor: change matrix.cpp

--- src/headers/matrix.cpp
+++ src/headers/matrix.cpp
@@ -29,18 +29,15 @@ TransportMatrix::TransportMatrix(vector<int> S, vector<vector<int>> C, vector<in
 
 
 
-ostream& operator<< (ostream out, TransportMatrix tm) {
-    // Заголовок таблицы
+ostream& operator<< (ostream& out, TransportMatrix& tm) {
     cout << left << setw(15) << "SOURCE";
+
     cout << left << setw(6 * tm.costs[0].size()) << "COST";
-//    for (int i = 0; i < tm.costs[0].size(); i++) {
-//    }
+
     cout << left << setw(8) << "SUPPLY" << endl;
 
-    // Разделитель
     cout << string(tm.costs[0].size() * 6 + 28, '-') << endl;
 
-    // Вывод строк таблицы
     int source_i = 1;
     for (int i = 0; i < tm.costs.size(); i++) {
       cout << left << setw(10) << source_i++;
@@ -50,10 +47,8 @@ ostream& operator<< (ostream out, TransportMatrix tm) {
       cout << right << setw(6) << tm.supply[i] << endl;
     }
 
-    // Разделитель
     cout << string(tm.costs[0].size() * 6 + 28, '-') << endl;
 
-    // Вывод строки DEMAND
     cout << left << setw(10) << "DEMAND";
     for (int i = 0; i < tm.demand.size(); i++) {
       cout << right << setw(6) << tm.demand[i];
